95adfab4b3befe9a037e9fcc31cd395b96e2c8c5
mustafashakaroun <moustafa.92@hotmail.com>
2018-02-05 23:33:23 +0200

    test

---
+++ ayoubStore/src/com/mustafa/main/Main.java
@@ -0,0 +1,35 @@
+package com.mustafa.main;
+
+public class Main {
+
+	
+	
+	
+	
+//	Welcome To Ayoub Store 
+//	Please enter your first name
+//	Ali
+//	Please enter your last name
+//	Ammar
+//	please enter you phone number
+//	70025207
+//	Welcome Ali Ammar , please select what you want
+//	1-show all products with salaries
+//	2-show products with offer
+//	3-show my cart
+//	4-select product
+//	5- pay
+//	-------------------------------------
+//	thanks for purchase from ayoub store
+//	2 : mouse                                                   22$
+//	1 : keyboard                                                15$
+//	1 : laptop Hp Pavillion 16gb ram , 1 Tb hdd , 240 ssd       900$
+//	total : 937$
+//	we hope that we will see you again :)
+	
+	public static void main(String[] args) {
+		
+
+	}
+
+}
